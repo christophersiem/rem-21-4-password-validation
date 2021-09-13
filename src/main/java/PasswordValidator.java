9b31f59575046dfb0e205f3b2b3277a8affb12c0
christophersiem <christopher.siem@gmail.com>
2021-09-13 10:00:22 +0200

    Add validate password

--- src/main/java/PasswordValidator.java
+++ src/main/java/PasswordValidator.java
@@ -1,6 +1,41 @@
 public class PasswordValidator {
 
-    public static boolean isLongEnough(String password){
+    public static boolean isValidPassword(String password) {
+        return isLongEnough(password) &&
+                containsDigit(password) &&
+                containsLowerCaseLetter(password) &&
+                containsUpperCaseLetter(password);
+    }
+
+    public static boolean isLongEnough(String password) {
         return password.length() >= 8;
+    }
+
+    public static boolean containsDigit(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            char symbol = password.charAt(i);
+            if (Character.isDigit(symbol)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsUpperCaseLetter(String password) {
+        return !password.equals(password.toLowerCase());
+    }
+
+
+    public static boolean containsLowerCaseLetter(String password) {
+        return !password.equals(password.toUpperCase());
+    }
+
+    public static boolean isValidPasswordList(String[] passwords) {
+        for (int i = 0; i < passwords.length; i++) {
+            if (!isValidPassword(passwords[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/PasswordValidatorTest.java
+++ src/test/java/PasswordValidatorTest.java
@@ -1,5 +1,10 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,12 +16,102 @@ class PasswordValidatorTest {
             "passwor, false",
             "password123, true"
     })
-    public void testIsLongEnough(String password, boolean expected){
+    public void testIsLongEnough(String password, boolean expected) {
         //WHEN
         boolean actual = PasswordValidator.isLongEnough(password);
 
         //THEN
         assertEquals(expected, actual);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "test1, true",
+            "password, false",
+            "password123, true"
+    })
+    public void testContainsDigit(String password, boolean expected) {
+        //WHEN
+        boolean actual = PasswordValidator.containsDigit(password);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "test1, false",
+            "passWord, true",
+            "passWORD, true",
+            "12345678, false",
+            "Üabcdefgh, true",
+    })
+    public void testContainsUppercaseLetter(String password, boolean expected) {
+        //WHEN
+        boolean actual = PasswordValidator.containsUpperCaseLetter(password);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "test1, true",
+            "passWord, true",
+            "PASSWORD, false"
+    })
+    public void testContainsLowercaseLetter(String password, boolean expected) {
+        //WHEN
+        boolean actual = PasswordValidator.containsLowerCaseLetter(password);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "testPW121, true",
+            "passWord, false",
+            "PASSWORD123, false",
+    })
+    public void testValidatePassword(String password, boolean expected) {
+        //WHEN
+        boolean actual = PasswordValidator.isValidPassword(password);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsValidPasswordList() {
+        //GIVEN
+        String[] passwords = {"passWORD1", "passWORD123"};
+
+        //WHEN
+        boolean actual = PasswordValidator.isValidPasswordList(passwords);
+
+        //THEN
+        assertTrue(actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getArguments")
+    public void testIsValidPasswordListMethodSource(String[] passwords, boolean expected) {
+
+        //WHEN
+        boolean actual = PasswordValidator.isValidPasswordList(passwords);
+
+        //THEN
+        assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> getArguments() {
+        return Stream.of(Arguments.arguments(
+                new String[]{"passwoRD123", "test123Hallo"}, true,
+                new String[]{"pasRD123", "test123Hallo"}, false,
+                new String[]{"passWORD", "test123Hallo"}, false
+        ));
+    }
+
 
 }
